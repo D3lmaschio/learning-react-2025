00fc250bc12ad4f9487352df6569b1a43a701123
ao32v <ao32v@cummins.com>
2025-05-20 15:56:34 -0300

    do trampo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header"
 
 export function App() {
   return (
     <>
+      <Header />
+
       <Post 
         author="Matheus"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero harum commodi inventore cupiditate modi sunt sint molestiae minus eius explicabo nisi iste nesciunt officia, obcaecati, quas, distinctio voluptatum unde?" 
       />
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post
+        author="Yasmin"
+        content="Lol"
+      />
     </>
   )
 }
