07324851decd99b6095ab94a432cc5bb7b5e75f5
ao32v <ao32v@cummins.com>
2025-05-20 14:04:35 -0300

    Learning React #1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <>
+      <Post 
+        author="Matheus"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero harum commodi inventore cupiditate modi sunt sint molestiae minus eius explicabo nisi iste nesciunt officia, obcaecati, quas, distinctio voluptatum unde?" 
+      />
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+    </>
+  )
+}
