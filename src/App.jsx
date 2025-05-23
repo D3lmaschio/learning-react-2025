a45cd8863ef7acf2710ee5e10919db1eede09e96
ao32v <ao32v@cummins.com>
2025-05-23 15:53:15 -0300

    DE SEXTA

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import { Post } from "./Post"
-import { Header } from "./components/Header"
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar"
+
+import styles from './App.module.css';
+
+import "./global.css";
 
 export function App() {
   return (
     <>
       <Header />
-
-      <Post 
-        author="Matheus"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero harum commodi inventore cupiditate modi sunt sint molestiae minus eius explicabo nisi iste nesciunt officia, obcaecati, quas, distinctio voluptatum unde?" 
-      />
-      <Post
-        author="Yasmin"
-        content="Lol"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Matheus"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit libero harum commodi inventore cupiditate modi sunt sint molestiae minus eius explicabo nisi iste nesciunt officia, obcaecati, quas, distinctio voluptatum unde?" 
+          />
+          <Post
+            author="Yasmin"
+            content="Lol"
+          />
+        </main>
+      </div>
     </>
-  )
+  );
 }
+;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import {  } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover}
+                src="https://i.pinimg.com/736x/4c/82/da/4c82dad04930f2622860b78c398c8908.jpg" 
+            />
+
+            <div className={styles.profile}>
+                 <img className={styles.avatar} src="https://github.com/D3lmaschio.png" />
+                <strong>D3lmaschio</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">Editar seu perfil</a>
+            </footer>
+        </aside>
+    );
+}
