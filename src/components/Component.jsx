efd5ae23c68c70cddcf436fe603365e01c568d22
D3lmaschio <matheus.rubens.ti@gmail.com>
2025-05-27 23:55:51 -0300

    de casa

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -4,7 +4,7 @@ export function Comment() {
   return (
     <div className={styles.commentBox}>
         <img className={styles.avatar} src="https://github.com/D3lmaschio.png" />
-        <div>
+        <div className={styles.commentContent}>
           <div className={styles.comment}>
             <strong>D3lmaschio</strong>
             <time title="26 de Maio às 15h53" dateTime="2025-05-26 15:50:00">Publicado há 1h</time>
