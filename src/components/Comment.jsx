e275254490f3f9ba4ee1ae805b4a5ebb4819b7f7
D3lmaschio <matheus.rubens.ti@gmail.com>
2025-05-28 22:39:44 -0300

    de casa

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -13,14 +13,14 @@ export function Comment() {
               <time title="26 de Maio às 15h53" dateTime="2025-05-26 15:50:00">Cerca de 1h atrás</time>
             </div>
             <button title="Delete comment">
-              <Trash size={20}/>
+              <Trash size={24}/>
             </button>
           </header>
           <p>Blá blá blá...</p>
         </div>
         <footer>
           <button>
-            <ThumbsUp />
+            <ThumbsUp size={16}/>
             Aplaudir <span>20</span>
           </button>
         </footer>
